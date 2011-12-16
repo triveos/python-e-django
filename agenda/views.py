bd996a837efadbd9835ab3046db1ec5bb836b81f
Gilson Filho <contato@gilsondev.com>
2011-12-16 19:22:30 -0200

    Actions de criar e editar item da agenda refatorados.

--- agenda/views.py
+++ agenda/views.py
@@ -21,19 +21,15 @@ def lista(request):
 
 @login_required
 def adiciona(request):
-    if request.method == 'POST': # Formulário enviado
-        form = FormItemAgenda(request.POST, request.FILES)
-        if form.is_valid():
-            # Formulário válido.
-            item = form.save(commit=False)
-            item.usuario = request.user
-            item.save()
-
-            # Mensagem de formulário cadastrado
-            return HttpResponseRedirect("/")
-    else:
-        # Exibe formulário em branco
-        form = FormItemAgenda()
+    form = FormItemAgenda(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        # Formulário válido
+        item = form.save(commit=False)
+        item.usuario = request.user
+        item.save()
+        
+        # Mensagem de formulário cadastrado
+        return HttpResponseRedirect("/")
 
     return render_to_response("adiciona.html", {'form': form},
             context_instance=RequestContext(request))
@@ -41,13 +37,10 @@ def adiciona(request):
 @login_required
 def item(request, nr_item):
     item = get_object_or_404(ItemAgenda, usuario=request.user, id=nr_item)
-    if request.method == "POST":
-        form = FormItemAgenda(request.POST, request.FILES, instance=item)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/")
-    else:
-        form = FormItemAgenda(instance=item)
+    form = FormItemAgenda(request.POST or None, request.FILES or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/")
     return render_to_response("item.html", {'form': form},
             context_instance=RequestContext(request))
 
